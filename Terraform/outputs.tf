4d5f8582d19b341f0ad37922bd61d327573cb221
Rajabrahmam Thipparthi <rajchary.rc79@gmail.com>
2023-02-01 18:36:40 +0530

    feat: Introduced Cache concept with Ansible actions

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -2,12 +2,6 @@ output "alb_endpoint" {
   value = module.loadbalancing.alb_dns
 }
 
-output "home_app_ip_address" {
-  value = module.compute.home_app_ip
-}
-output "products_app_ip_address" {
-  value = module.compute.products_app_ip
-}
 output "inventory" {
   value = templatefile(
     "${path.module}/templates/inventory.tpl",
