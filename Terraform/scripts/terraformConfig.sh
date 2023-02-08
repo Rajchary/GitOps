12d148e76ce07b20a7160d68641ea9b53cadc4bb
Rajabrahmam Thipparthi <rajchary.rc79@gmail.com>
2023-02-08 12:49:02 +0530

    feat: dedicated scripts added

--- Terraform/scripts/terraformConfig.sh
+++ Terraform/scripts/terraformConfig.sh
@@ -1,3 +1,3 @@
-echo 'plugin_cache_dir="$HOME/.terraform.d/plugin-cache"' >~/.terraformrc
-echo -e 'credentials "app.terraform.io" {\ntoken = ${{ secrets.TF_API_TOKEN }}\n}' >> ~/.terraformrc
+echo "plugin_cache_dir=\"$HOME/.terraform.d/plugin-cache\"" >~/.terraformrc
+echo "credentials \"app.terraform.io\" {\ntoken = $TF_API_TOKEN \n}" >> ~/.terraformrc
 mkdir --parents ~/.terraform.d/plugin-cache
